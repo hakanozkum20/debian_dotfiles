738d4f86378541a8f465d1a8ba6d9fb80f4ae5ae
Hakan ÖZKUM <hakanozkum20@gmail.com>
2022-01-10 15:59:23 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,6 +18,19 @@ apt update
 apt install unzip picom bspwm polybar sddm rofi kitty thunar flameshot neofetch sxhkd git lxpolkit lxappearance xorg -y
 apt install papirus-icon-theme lxappearance fonts-noto-color-emoji fonts-firacode fonts-font-awesome libqt5svg5 qml-module-qtquick-controls -y
 
+#Local Folders
+mkdir -p ~/.themes ~/.fonts ~/.config ~/Documents ~/Downloads ~/Downloads/build ~/Pictures ~/Pictures/Screenshots ~/Videos 
+
+#Getting Qoqir theem
+apt-get install gtk2-engines-murrine gtk2-engines-pixbuf
+cd /home/esta/Downloads/
+git clone https://github.com/vinceliuice/Qogir-theme.git
+cd /Qoqir-theme/
+chmod +x install.sh
+./install.sh -c dark -t standard
+
+
+
 # Download Nordic Theme
 cd /usr/share/themes/
 git clone https://github.com/EliverLara/Nordic.git
@@ -27,10 +40,13 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/FiraCode.z
 unzip FiraCode.zip -d /usr/share/fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/Meslo.zip
 unzip Meslo.zip -d /usr/share/fonts
+wget https://github.com/be5invis/Iosevka/releases/download/v11.2.6/ttf-iosevka-11.2.6.zip
+unzip ttf-iosevka-11.2.6.zip -d /usr/share/fonts
 fc-cache -vf
 
 
 # Get Rofi Theme Fonts
+apt-get install font-manager
 cd ~/Downloads/
 git clone https://github.com/adi1090x/rofi
 cd rofi/fonts
