660ee79cad8f61ca232211108a2dceaacfb8eafb
Hakan ÖZKUM <hakanozkum20@gmail.com>
2022-01-13 20:51:12 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -75,7 +75,8 @@ mkdir -p /home/$username/.config
 cp -R .config/* /home/$username/.config/
 chown -R $username:$username /home/$username
 chmod +x /home/$username/.screenlayout/monitor.sh /home/$username/.config/bspwm/* /home/$username/.config/bspwm/scripts/* /home/$username/.config/bspwm/scripts/i3lock-fancy/* /home/$username/.config/dunst/* /home/$username/.config/kitty/* /home/$username/.config/polybar/* /home/$username/.config/polybar/scripts/* /home/$username/.config/rofi/* /home/$username/.config/sxhkd/* /home/$username/.config/picom.conf
-cd /Debian_dotfiles
+cd /home/$username/Debian_dotfiles
 cp -R background.jpg /home/$username/Pictures/
+nitrogen --set-auto /home/$username/Pictures/background.jpg
 reboot
 
